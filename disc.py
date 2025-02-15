54e8e8b68a211697157a2920040a3034ef731de0
Pearlsoldier <marumotodaiki@gmail.com>
2025-02-16 01:24:14 +0900

    Initial board set

--- disc.py
+++ disc.py
@@ -1,16 +1,7 @@
 class Disc():
     """
-    石クラス
-    白石、黒石の親になるクラス
+    石を置いたときに、8方向を見る。
+    置いたとこを絶対値0として、左方向、上方向を-,右方向、下方向を+とする
     """
-    def __init__(self, coler):
-        self.coler = coler
-        
-    # memo
-    # 石を置いたときに、8方向を見る。
-    # 置いたとこを絶対値0として、左方向、上方向を-,右方向、下方向を+とする
-    # wd = Disc("⚪️") #白丸
-    # print(*wd.coler)
-
-    # bd  = Disc("⚫️") #黒丸
-    # print(*bd.coler)
+    def __init__(self, color):
+        self.color = color
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import board
 import disc
 
-test = board.Board()
-wd = disc.Disc("White")
 
-print(*test.row, sep="\n")
-print(wd.coler("⚪️"))
+wd = disc.Disc("⚪️")
+wd.color
+bd = disc.Disc("⚫️")
+bd.color
+
+test = board.Board()
+test.row[4][4] = wd.color
+test.row[4][5] = bd.color
+test.row[5][4] = bd.color
+test.row[5][5] = wd.color
+
+for i in range(len(test.row)):
+    print(*test.row[i])
+
