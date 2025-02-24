9ef38f651c16eff80473995278aa884d0dce1759
Pearlsoldier <marumotodaiki@gmail.com>
2025-02-24 23:04:04 +0900

    Check adjacent cells in 8 directions

--- rule.py
+++ rule.py
@@ -10,8 +10,26 @@ class Rule:
         pass
 
     def is_adjacent_cells_filled(self, row: int, column: int, board, color) -> bool:
-        if not (board.row[column][row + 1] == color):
-            if not (board.row[column][row + 1] == "-"):
+        if (
+            not (board.row[column][row - 1] == color)
+            or not (board.row[column + 1][row - 1] == color)
+            or not (board.row[column + 1][row] == color)
+            or not (board.row[column + 1][row + 1] == color)
+            or not (board.row[column][row + 1] == color)
+            or not (board.row[column - 1][row + 1] == color)
+            or not (board.row[column - 1][row] == color)
+            or not (board.row[column - 1][row - 1] == color)
+        ):
+            if (
+                not (board.row[column][row - 1] == "-")
+                or not (board.row[column + 1][row - 1] == "-")
+                or not (board.row[column + 1][row] == "-")
+                or not (board.row[column + 1][row + 1] == "-")
+                or not (board.row[column][row + 1] == "-")
+                or not (board.row[column - 1][row + 1] == "-")
+                or not (board.row[column - 1][row] == "-")
+                or not (board.row[column - 1][row - 1] == "-")
+            ):
                 return True
             else:
                 return False
