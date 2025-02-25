60891f6f606503aac4ae1a5b4cad65711d72bc83
nuco-mrmt <daiki_marumoto@nuco.co.jp>
2025-02-25 20:47:24 +0900

    Change color_naming

--- main.py
+++ main.py
@@ -4,41 +4,44 @@ from player import Player
 from situation import Situation
 from rule import Rule
 
-board = Board()
-rows = board.row  # 座標を示す
-wd = Disc("⚪️")
-wd.color
-bd = Disc("⚫️")
-bd.color
-
-situ = Situation()
-player = Player()
-rules = Rule()
-
 
 def main():
+    bd = Disc("⚫️")
+    wd = Disc("⚪️")
+    board = Board()
+    rows = board.row
+    situ = Situation()
+    player = Player()
+    rules = Rule()
+
     print("オセロを始めます。")
+    print("======================")
     for i in range(len(rows)):
         print(*rows[i])
+    print("======================")
     for i in range(1, 61):
         situ.turn_count += 1
         if situ.is_black_turn:
-            color = bd.color
-            print(f"先手,{color}。")
+            current_color = bd.color  # 現在の色
+            opposite_color = wd.color  # 相手（反対）の色
+            print(f"先手,{current_color}。")
         else:
-            color = wd.color
-            print(f"後手,{color}。")
+            current_color = wd.color
+            opposite_color = bd.color
+            print(f"後手,{current_color}。")
 
         row, column = situ.put_disc(input())
         print(row, column)
         is_legal = rules.is_legal_cell(row, column, board)
         if is_legal:
-            is_legal = rules.is_adjacent_cells_filled(row, column, board, color)
+            is_legal = rules.is_adjacent_cells_filled(row, column, board, current_color)
             if is_legal:
-                refreshed_board = player.move(row, column, color, board)
+                refreshed_board = player.move(row, column, current_color, board)
                 situ.reflesh_board(refreshed_board)
                 for i in range(len(rows)):
                     print(*rows[i])
+                print("======================")
+
             else:
                 while not is_legal:
                     print(f"❌手 {row, column} は石を置けません❌")
@@ -48,15 +51,16 @@ def main():
                     row, column = situ.put_disc(input())
                     if rules.is_legal_cell(row, column, board):
                         is_legal = rules.is_adjacent_cells_filled(
-                            row, column, board, color
+                            row, column, board, current_color
                         )
                     print(f"ac :{is_legal}")
                     if is_legal:
                         print(f"🙆手 {row, column} は有効です🙆")
-                        refreshed_board = player.move(row, column, color, board)
+                        refreshed_board = player.move(row, column, current_color, board)
                         situ.reflesh_board(refreshed_board)
                         for i in range(len(rows)):
                             print(*rows[i])
+                        print("======================")
                         continue
 
         else:
@@ -65,16 +69,20 @@ def main():
                 print("もう一度入力してください！！")
                 for i in range(len(rows)):
                     print(*rows[i])
+                print("======================")
                 row, column = situ.put_disc(input())
                 if rules.is_legal_cell(row, column, board):
-                    is_legal = rules.is_adjacent_cells_filled(row, column, board, color)
+                    is_legal = rules.is_adjacent_cells_filled(
+                        row, column, board, current_color
+                    )
                 print(f"ac :{is_legal}")
                 if is_legal:
                     print(f"🙆手 {row, column} は有効です🙆")
-                    refreshed_board = player.move(row, column, color, board)
+                    refreshed_board = player.move(row, column, current_color, board)
                     situ.reflesh_board(refreshed_board)
                     for i in range(len(rows)):
                         print(*rows[i])
+                    print("======================")
                     continue
 
 
--- rule.py
+++ rule.py
@@ -1,19 +1,38 @@
 class Rule:
     def __init__(self):
+        """
+        1.-であること
+        2.隣に自分ではない色があること。
+        3.２つ以上連続して石があること
+        4.連続する石の先が自分の石であること
+        """
         pass
 
     def is_legal_cell(self, row: int, column: int, board) -> bool:
-        return board.row[row][column] == "-"
+        """
+        1.-であること
+        石が置けるブランク状態かの判定
+        """
+        if board.row[row][column] == "-":
+            return True
 
-    def flip_line(self):
 
-        pass
 
     def is_adjacent_cells_filled(self, row: int, column: int, board, color) -> bool:
-        if not (board.row[column][row + 1] == color):
-            if not (board.row[column][row + 1] == "-"):
-                return True
-            else:
-                return False
-        else:
-            return False
+            """
+            2.隣に自分ではない色があること。
+            """
+            return  not ((board.row[column][row] == color) and (board.row[i][row] == "-"))
+            
+
+
+    def is_valid_flank_capture(self, row: int, column: int, board, color) -> bool:
+        """
+        1方向に対して挟み込み捕獲が有効かどうかの判定
+        行（列）末まで確認する。
+        置いた位置から、石が連続しているか
+        最後の石が自分の石か
+        途中で - があれば、判定はFalseになる。
+        """
+        pass
+        
